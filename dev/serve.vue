0d8eaff75251ef03901f3960812c7558baf436d1
Demetrios Collier <demetrios.collier@safesize.com>
2022-06-06 14:18:42 +0300

    Init SFC and forked vue-barcode-reader

---
+++ dev/serve.vue
@@ -0,0 +1,22 @@
+<script>
+import Vue from "vue";
+import ImageBarcodeScanner from "../src/lib-components/ImageBarcodeScanner.vue";
+import StreamBarcodeScanner from "../src/lib-components/StreamBarcodeScanner.vue";
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { ImageBarcodeScanner } from "@/entry.esm";
+
+export default Vue.extend({
+  components: { ImageBarcodeScanner, StreamBarcodeScanner },
+  name: "ServeDev",
+  // components: {
+  //   ImageBarcodeScanner,
+  // },
+});
+</script>
+
+<template>
+  <div id="app">
+    <ImageBarcodeScanner />
+    <StreamBarcodeScanner />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,3 @@
+/* eslint-disable import/prefer-default-export */
+export { ImageBarcodeScanner } from "./ImageBarcodeScanner.vue";
+export { StreamBarcodeScanner } from "./StreamBarcodeScanner.vue";
